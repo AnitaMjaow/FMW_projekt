0858f023afcaeb0b1786c425658c57d66d60e5b8
AnitaMjaow <olsson_anita@live.se>
2023-05-24 12:57:36 +0200

    installation of react app + added api.js file

---
+++ my-app/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+		"X-RapidAPI-Key": "354b2119a3mshc4ee7f7e0d0ede7p1b6ebdjsnf0949e5ee07a",// enter your rapid api key here
+		"X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0da93fc7ce91a88806bb6497b840ea61"; // enter your key from openweather API
