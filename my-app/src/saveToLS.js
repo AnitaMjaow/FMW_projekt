2a32f4d837820085766a6ec2c265d4e4dbef7af5
Mr E <98057564+ExtremeMalmz@users.noreply.github.com>
2023-05-26 21:57:51 +0200

    You can store location in local storage now

--- my-app/src/saveToLS.js
+++ my-app/src/saveToLS.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 const Button = ({ searchValue }) => {
     const handleClick = () => {
-      //console.log('Search value:', searchValue);
-    
-      //save the search value to local storage
-    
+      console.log("This the search value:" + searchValue);
 
-    };
+      if(searchValue !== ""){
+        //this means its basically a day
+      var twentyFourHours = new Date().getTime() + 24 * 60 * 60 * 1000; 
+      var data = { value: searchValue, twentyFourHours };
+
+      //setting it to localstorage here
+      localStorage.setItem("searchData", JSON.stringify(data));
+      }
+      else{
+        //if its empty do nothing
+        console.log(":( empty");
+      }
+    };  
   
   return (
     <button onClick={handleClick}>Click me to save your current location</button>
