8fef18d1c3ed0c92e701d7a6c29c47d9ee594f63
AnitaMjaow <olsson_anita@live.se>
2023-06-03 18:09:35 +0200

    added changes to the last main commit

--- my-app/src/components/local-storage/loadfromLS.js
+++ my-app/src/components/local-storage/loadfromLS.js
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
 
-const LoadFromLSButton = ()   => {
+const LoadFromLSButton = () => {
   //const [setButtonText] = useState('Click to see a history of your saved searches');
   const [listItems, setListItems] = useState([]);
 
-    const handleClick = () => {
-      //if theres stuff in search data
-        if(localStorage.getItem("searchData") !== null){ 
-            //fetch data from Local storage
-            const dataInLocalStorage = localStorage.getItem("searchData");    
-            
-            const parsedData = JSON.parse(dataInLocalStorage);
-            const values = parsedData.map((item, index) => (
-              <li key={index}>{item.value}<button onClick={() => handleDelete(index)}>X</button></li>
-            ));
+  const handleClick = () => {
+    //if theres stuff in search data
+    if (localStorage.getItem("searchData") !== null) {
+      //fetch data from Local storage
+      const dataInLocalStorage = localStorage.getItem("searchData");
 
-            //set it to the ul
-            setListItems(values);
-        }
-        else{
-            alert("Nothing is in the loaded data");
-        }
+      const parsedData = JSON.parse(dataInLocalStorage);
+      const values = parsedData.map((item, index) => (
+        <li id="list-item" key={index}>{item.value}<span id="delete-list-item" onClick={() => handleDelete(index)}>X</span></li>
+      ));
+
+      //set it to the ul
+      setListItems(values);
+    }
+    else {
+      alert("Nothing is in the loaded data");
+    }
 
     //this deletes the list item
     const handleDelete = (index) => {
@@ -31,16 +31,16 @@ const LoadFromLSButton = ()   => {
       //update the local storage
       localStorage.setItem('searchData', JSON.stringify(updatedData));
       const updatedValues = updatedData.map((item, i) => (
-      <li key={i}>
-        {item.value}
-        <button onClick={() => handleDelete(i)}>X</button>
-      </li>
-    ));
+        <li id="list-item" key={i}>
+          {item.value}
+          <span id="delete-list-item" onClick={() => handleDelete(i)}>X</span>
+        </li>
+      ));
 
-    //update the UL with list items
-    setListItems(updatedValues);
-  }
+      //update the UL with list items
+      setListItems(updatedValues);
     }
+  }
   return (
     <div>
       <button onClick={handleClick}>Click to see a history of your saved searches</button>
